49bbd2c3222259d772242301b7f9e93e314b8297
Pongpon Sinlapa <mr.pongpon.sinlapa.m.m@gmail.com>
2019-10-05 16:49:26 +0700

    First commit

--- index.php
+++ index.php
@@ -51,7 +51,7 @@
         
         $arrayPostData['replyToken'] = $arrayJson['events'][0]['replyToken'];
         $arrayPostData['messages'][0]['type'] = "text";
-        $arrayPostData['messages'][0]['text'] = $a[$random_keys[0]];
+        $arrayPostData['messages'][0]['text'] = implode("",$a[$random_keys[0]]));
         $arrayPostData['messages'][1]['type'] = "sticker";
         $arrayPostData['messages'][1]['packageId'] = "1";
         $arrayPostData['messages'][1]['stickerId'] = "131";
